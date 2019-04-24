ec191e9717f500144ee54f2ad06ce47dd573ed2b
Silent Sonata <musictechy.cb@gmail.com>
2019-04-24 16:01:14 -0400

    Updated live section

--- lib/pages/live_section.dart
+++ lib/pages/live_section.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class LiveSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Column(
-      children: <Widget>[
-        Image.asset('assets/prayer_walk-background.jpg'),
-      ],
+      children: <Widget>[],
     );
   }
 }
